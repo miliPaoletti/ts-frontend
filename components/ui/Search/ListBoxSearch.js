4c18f67832ec1476edb42c2931af0bc57aa58fe2
miliPaoletti <milagros.paoletti@gmail.com>
2022-08-22 16:26:13 +0200

    delete console and change firebase credentials

--- components/ui/Search/ListBoxSearch.js
+++ components/ui/Search/ListBoxSearch.js
@@ -3,7 +3,6 @@ import { Listbox, Transition } from "@headlessui/react";
 import { BsCheck } from "react-icons/bs";
 import { TbSelector } from "react-icons/tb";
 const ListBoxSearch = ({ data, separator, icon, onChange, text, selected }) => {
-  console.log(selected);
   const onChangeSetSelected = (q) => {
     onChange(q);
   };
