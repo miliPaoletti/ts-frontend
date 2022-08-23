545def55b98783983ed4d1c8f272e7010fb388d7
miliPaoletti <milagros.paoletti@gmail.com>
2022-08-23 19:04:51 +0200

    open new tab when click on Buscar

--- components/ui/Search/SearchContainer.js
+++ components/ui/Search/SearchContainer.js
@@ -7,6 +7,7 @@ import { GoCalendar } from "react-icons/go";
 
 import InputContainer from "components/ui/Search/InputContainer";
 import ListBoxSearch from "components/ui/Search/ListBoxSearch";
+import Link from "next/link";
 
 const getDestinationNames = (destinations) => {
   return Object.keys(destinations);
@@ -47,17 +48,6 @@ const SearchBar = ({ destinationsNames, months, destination, month }) => {
     }
   }, [selectedDestination, months, destinationsNames]);
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    router.push({
-      pathname: `/search`,
-      query: {
-        destination: selectedDestination,
-        month: selectedMonth,
-      },
-    });
-  };
-
   return (
     <div className="relative mt-0 md:flex md:items-center md:justify-center md:w-full md:left-0 z-[100] md:mt-[-120px]">
       <div className=" shadow-[0_0_15px_0px] shadow-orange-400">
@@ -87,14 +77,18 @@ const SearchBar = ({ destinationsNames, months, destination, month }) => {
               />
             }
           />
-          <form
-            className="relative overflow-hidden z-30 w-full md:w-auto"
-            onSubmit={handleSubmit}
+          <Link
+            href={{
+              pathname: "search",
+              query: { destination: selectedDestination, month: selectedMonth },
+            }}
           >
-            <button className="uppercase p-2 md:p-5 text-sm md:text-xl text-white font-bold bg-orange-950 md:bg-transparent w-full md:w-auto md:before:-z-50 md:before:content-[''] md:before:absolute md:before:right-[-6px] md:before:top-0 md:before:h-full md:before:w-full md:before:-skew-x-6 md:before:bg-orange-950 remove-selection">
-              buscar
-            </button>
-          </form>
+            <a className="relative overflow-hidden z-30 w-full md:w-auto">
+              <div className="uppercase p-2 md:p-5 text-sm md:text-xl text-white font-bold bg-orange-950 md:bg-transparent w-full md:w-auto md:before:-z-50 md:before:content-[''] md:before:absolute md:before:right-[-6px] md:before:top-0 md:before:h-full md:before:w-full md:before:-skew-x-6 md:before:bg-orange-950 remove-selection">
+                buscar
+              </div>
+            </a>
+          </Link>
         </div>
       </div>
     </div>
