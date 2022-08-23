e3c15402558f237d6e3a718127a6444db229644b
miliPaoletti <milagros.paoletti@gmail.com>
2022-08-23 20:36:39 +0200

    add hover to whatsapp button

--- components/ui/Links/Whatsapp.js
+++ components/ui/Links/Whatsapp.js
@@ -5,7 +5,7 @@ import { BsWhatsapp } from "react-icons/bs";
 
 const Whatsapp = () => {
   return (
-    <div className="rounded-full bg-green-400 sticky bottom-10 left-10 w-fit p-3  z-[9999]">
+    <div className="rounded-full bg-green-400 sticky bottom-10 left-10 w-fit p-3  z-[9999] hover:bg-green-500 transition transform duration-200 ease-out">
       <Link href={URL_WHATSAPP}>
         <a rel="noreferrer" target="_blank">
           <BsWhatsapp className="text-4xl text-white" />
