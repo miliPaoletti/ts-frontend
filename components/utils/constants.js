296728f6edd8cdea06aa964abc12f66a01958c63
miliPaoletti <milagros.paoletti@gmail.com>
2022-08-24 12:42:02 +0200

    fix img_default with process.env

--- components/utils/constants.js
+++ components/utils/constants.js
@@ -18,7 +18,7 @@ export const months = [
 export const IMG_DEFAULT = `${
   process.env.NEXT_PUBLIC_BASE_PATH === undefined
     ? "default_img.jpg"
-    : `url(${process.env.NEXT_PUBLIC_BASE_PATH}/default_img.jpg)`
+    : `${process.env.NEXT_PUBLIC_BASE_PATH}/default_img.jpg`
 }`;
 
 export const RESULTS_SEARCH = 6;
